0433de1fc77302daffb32cc138dcfc4afdb727f9
Nico Kaiser <nico@kaiser.me>
2022-07-12 12:47:46 +0200

    Based on version bundled in Jetpack 11.1.1

--- carousel-without-jetpack.php
+++ carousel-without-jetpack.php
@@ -8,23 +8,3 @@ Author: Nico Kaiser
 */
 
 include dirname( __FILE__ ) . '/carousel/jetpack-carousel.php';
-
-add_action( 'wp_loaded', function() {
-    if ( ! wp_script_is( 'spin', 'registered' ) ) {
-        wp_register_script(
-            'spin',
-            plugins_url( '_inc/build/spin.min.js', __FILE__ ),
-            false,
-            '1.3'
-        );
-    }
-
-    if ( ! wp_script_is( 'jquery.spin', 'registered' ) ) {
-        wp_register_script(
-            'jquery.spin',
-            plugins_url( '_inc/build/jquery.spin.min.js', __FILE__ ),
-            array( 'jquery', 'spin' ),
-            '1.3'
-        );
-    }
-} );
